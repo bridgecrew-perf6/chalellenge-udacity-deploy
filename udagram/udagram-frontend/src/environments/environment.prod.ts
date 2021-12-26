8bc45e8e497ce7157cf82a5b1d3f1dcab46d4e45
kevin farias <kevin.farias.dev@gmail.com>
2021-12-26 14:43:32 -0300

    new version

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://udacitytest-env.eba-k8amwfpu.us-east-1.elasticbeanstalk.com:8080/api/v0",
 };
 
 /*
