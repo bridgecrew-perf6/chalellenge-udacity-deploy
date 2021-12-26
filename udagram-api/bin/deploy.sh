32c9b0dac443c8370fc5d8b184d9ae6ccf84fb08
Kevin Farias <kevin.farias.dev@gmail.com>
2021-12-26 15:36:12 -0300

    changing pipeline

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,1 @@
-eb init udacity-test && eb deploy
+printf '\n\n\n\n\n' && eb init udacity-test --region us-east-1 --platform "Node.js 14 running on 64bit Amazon Linux 2" && eb deploy Udacitytest-env
